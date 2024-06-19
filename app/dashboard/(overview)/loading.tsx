26bb9d331e2f15f754c308f9a21c32ebdcaed20f
SpaceDubby07 <zclark006@outlook.com>
2024-06-19 12:47:56 -0400

    3

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,5 +1,6 @@
 import DashboardSkeleton from '@/app/ui/skeletons';
 
 export default function Loading() {
+  // skeleton that loads before the data if the data takes time to load
   return <DashboardSkeleton />;
 }
